c4b2b35f2aab323cc79ce6d987422dc97a996de7
Ion Stici <ionsticidev@gmail.com>
2023-01-22 17:48:13 +0200

    Add leaflet and back button

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+var map = L.map("map").setView([41.480416, -71.31093], 15.4);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([41.480416, -71.31093]).addTo(map);
